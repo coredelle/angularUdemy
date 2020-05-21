a1c7671e65456303edf6a83675085dfefdf53e5b
coredelle <westga09@gmail.com>
2020-05-21 08:46:02 -0400

    use routerlink to make routes work dynamically

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,12 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  @Output() navigation = new EventEmitter<string>();
-  collapsed = true;
+export class HeaderComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
-  navigate(naviString: string) {
-    this.navigation.emit(naviString);
-  }
 }

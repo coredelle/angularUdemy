a0ab51011306af324059af0e83dddb9ffa56f76a
coredelle <westga09@gmail.com>
2020-06-01 20:14:21 -0400

    improve form by using ngForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {IngredientModel} from '../../../models/ingredient.model';
+import {Component, OnInit} from '@angular/core';
 import {ShoppingListService} from '../shopping-list.service';
+import {NgForm} from '@angular/forms';
+import {IngredientModel} from '../../../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,16 +9,15 @@ import {ShoppingListService} from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    const newIngredient = new IngredientModel(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new IngredientModel(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
   }
 

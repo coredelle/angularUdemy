35e5121b6b727f67b6ccd259e7cef103739793ae
coredelle <westga09@gmail.com>
2020-06-04 09:08:29 -0400

    refactor code to use interceptor to add token to all requests. much cleaner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,9 +16,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
+import {AuthInterceptorService} from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,13 @@ import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [RecipeService, ShoppingListService],
+  providers: [RecipeService, ShoppingListService,
+    {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+    multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {RecipeModel} from '../../models/recipe.model';
-import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import {AuthService} from '../auth/auth.service';
 
 @Injectable({
@@ -17,15 +17,8 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    // using (take) here allows for taking one value from the subscription then immediately unsubscribing after
-    return this.authService.user.pipe(take(1),
-      exhaustMap(user => {
-        return this.http.get<RecipeModel[]>('https://ng-complete-guide-2e7e9.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          });
-    }),
-      map(recipes => {
+    return this.http.get<RecipeModel[]>('https://ng-complete-guide-2e7e9.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
         });
